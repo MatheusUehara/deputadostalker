3756a4d43b76c12f8d1fe3c618677477a81809d1
Matheus Uehara <matheuspitanco@gmail.com>
2016-05-24 00:55:40 -0300

    Commit com correcao de merge

--- app/src/main/java/app/deputadostalker/comissao/api/ComissaoAPI.java
+++ app/src/main/java/app/deputadostalker/comissao/api/ComissaoAPI.java
@@ -3,7 +3,6 @@ package app.deputadostalker.comissao.api;
 import java.util.List;
 
 import app.deputadostalker.comissao.dominio.Comissao;
-import app.deputadostalker.partido.dominio.Partido;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
